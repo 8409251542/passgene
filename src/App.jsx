d029c0fd307cfa1b6434fcf80d383da766c75db1
Ravi Shankar <ravi.yoof@gmail.com>
2024-05-08 23:22:06 +0530

    started

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="maincontainer">
+      <div className="main">
+        <div className="header">
+          <div className="pw">
+            Kgth85%4*gJYT
+          </div>
+          <button className="copyBtn">Copy</button>
+        </div>
+        <div className="slider">
+          <span>
+            <label htmlFor="">Charecter Length</label>
+            <label htmlFor="">4</label>
+          </span>
+          <input type="range" />
+        </div>
+      
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
